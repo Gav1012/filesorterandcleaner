1390669f15a9b4c15709a868d406b9656c469a24
Gav1012 <gavin.poley@gmail.com>
2024-08-23 23:54:23 -0700

    added functions to create a folder if not there and move file to said folder

--- downloads.py
+++ downloads.py
@@ -4,6 +4,14 @@ from pathlib import Path
 
 downloads_folder = Path.home() / 'Downloads'
 
+def create_folder(folder_path):
+    if not folder_path.exists():
+        folder_path.mkdir(parents=True, exist_ok=True)
+
+def move_file(file, destination_folder):
+    create_folder(destination_folder)
+    shutil.move(str(file), str(destination_folder / file.name))
+
 for file in downloads_folder.iterdir():
     if file.is_file():
         print(file.name)
